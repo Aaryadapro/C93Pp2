d253364e021d8e8cd5e40bfc5151c3a61f31c09d
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-05-25 18:19:51 +1200

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+function addUser(){
+username = document.getElementById("login_input").value;
+localStorage.setItem("username", username);
+
+window.location = "kwitter_room.html";
+}
+
+//ke the username from the input box, store it in localStorage and then redirect it to kwitter_room.html page.
+username = document.getElementById("login_input").value;
+localStorage.setItem("username", username);
+window.location = "kwitter_room.html";
